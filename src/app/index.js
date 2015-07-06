be976aae61bca28f0e23747ea56a4e5cd1a27f71
Patrick Towles <towlesj@nku.edu>
2015-07-05 21:35:11 -0400

    Initial commit

---
+++ src/app/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import MainCtrl from './main/main.controller';
+import NavbarCtrl from '../components/navbar/navbar.controller';
+
+angular.module('trackit', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'mgcrea.ngStrap', 'angular-loading-bar'])
+  .controller('MainCtrl', MainCtrl)
+  .controller('NavbarCtrl', NavbarCtrl)
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
